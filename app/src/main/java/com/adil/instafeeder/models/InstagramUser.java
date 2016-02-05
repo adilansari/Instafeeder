a802b80e57bfd864836be303c9515760fb2f67f8
Adil Ansari <adil@chartboost.com>
2016-02-05 00:20:57 -0800

    Profile layout ready

--- app/src/main/java/com/adil/instafeeder/models/InstagramUser.java
+++ app/src/main/java/com/adil/instafeeder/models/InstagramUser.java
@@ -1,7 +1,22 @@
 package com.adil.instafeeder.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by adil on 2/2/16.
  */
 public class InstagramUser {
+    public String username;
+    public String imageUrl;
+
+    public static InstagramUser fromJson(JSONObject jsonObject) throws JSONException {
+        if (jsonObject == null)
+            return null;
+        InstagramUser instagramUser = new InstagramUser();
+        instagramUser.username = jsonObject.getString("username");
+        instagramUser.imageUrl = jsonObject.getString("profile_picture");
+
+        return instagramUser;
+    }
 }

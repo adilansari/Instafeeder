9c7c15f8a0399581a35488a2460ee24a69e2d720
Adil Ansari <ansari.adil20@gmail.com>
2016-02-05 14:34:38 -0800

    adding count text

--- app/src/main/java/com/adil/instafeeder/adapters/InstagramPostsAdapter.java
+++ app/src/main/java/com/adil/instafeeder/adapters/InstagramPostsAdapter.java
@@ -37,25 +37,32 @@ public class InstagramPostsAdapter extends ArrayAdapter<InstagramPost>{
         }
 
         InstagramPost post = getItem(position);
+
         TextView tvUserName = (TextView) v.findViewById(R.id.username);
-        TextView tvTime = (TextView) v.findViewById(R.id.timeDiff);
-
-        ImageView imgView = (ImageView) v.findViewById(R.id.ivPhoto);
-        ImageView imgProfile = (ImageView) v.findViewById(R.id.ivProfilePicture);
-
-        TextView tvLikesCount = (TextView) v.findViewById(R.id.likesCount);
-        TextView tvCaption = (TextView) v.findViewById(R.id.tvCaption);
-
         tvUserName.setText(post.user.username);
+
+        TextView tvTime = (TextView) v.findViewById(R.id.timeDiff);
         tvTime.setText(Utils.getRelativeTimeSpan(post.createdTime));
+
+        ImageView imgProfile = (ImageView) v.findViewById(R.id.ivProfilePicture);
         imgProfile.setImageResource(0);
         Picasso.with(getContext()).load(post.user.imageUrl).into(imgProfile);
 
+        ImageView imgView = (ImageView) v.findViewById(R.id.ivPhoto);
         imgView.setImageResource(0);
         Picasso.with(getContext()).load(post.imageUrl).into(imgView);
 
-        tvCaption.setText(post.caption);
+        TextView tvLikesCount = (TextView) v.findViewById(R.id.likesCount);
         tvLikesCount.setText(Utils.templatifyLikesCount(post.likesCount));
+
+        TextView tvCaptionUsername = (TextView) v.findViewById(R.id.tvCaptionUsername);
+        tvCaptionUsername.setText(post.user.username);
+
+        TextView tvCaption = (TextView) v.findViewById(R.id.tvCaption);
+        tvCaption.setText(post.caption);
+
+        TextView tvCommentsCount = (TextView) v.findViewById(R.id.tvCommentsCount);
+        tvCommentsCount.setText(Utils.templatifyCommentsCount(post.commentsCount));
 
         Log.i(TAG, "likesCount: " + post.likesCount + " caption: " + post.caption + " commentsCount: " + post.commentsCount + " user: " + post.user);
 
